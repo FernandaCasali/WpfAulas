b11aa29e1994f9ca4496a5c41f00f61d4bef94d9
FernandaCasali <fernanda.t.casali@gmail.com>
2025-06-27 19:01:21 -0300

    Adicionar arquivos de projeto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfApp2
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
