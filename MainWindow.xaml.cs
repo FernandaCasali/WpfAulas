b39551e669569f67bf815dfb5571c3f06ee4e2f2
FernandaCasali <fernanda.t.casali@gmail.com>
2025-07-02 18:39:28 -0300

    batao como YesNo e question

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,19 @@ namespace WpfApp2
 
         private void btnFire_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Could not open file", "ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
+            //MessageBox.Show("Could not open file", "ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            MessageBoxResult result = MessageBox.Show
+                ("Do you agree?", "Agreement", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                tbinfo.Text = "Agreed";
+            }
+            else
+            {
+                tbinfo.Text = "Disagreed";
+            }
         }
     }
 }
