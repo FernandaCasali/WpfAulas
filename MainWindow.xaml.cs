3ac0ece05dedc6e625fb70ce1eee784fea12930e
FernandaCasali <fernanda.t.casali@gmail.com>
2025-07-02 18:27:04 -0300

    xaml e cs iniciais

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,39 +5,14 @@ using System.Windows;
 namespace WpfApp2
 {
 
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
 
         public MainWindow()
         {
-            DataContext = this;
             InitializeComponent();
             
         }
 
-
-        private string boundText;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public string BoundText
-        {
-            get { return boundText; }
-            set 
-            { 
-                boundText = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private void tbSet_Click(object sender, RoutedEventArgs e)
-        {
-            BoundText = "set from code";
-        }
-
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
