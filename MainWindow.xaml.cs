95b1d3c975f1584fae10b358b9699aca2e9f5475
FernandaCasali <fernanda.t.casali@gmail.com>
2025-07-07 16:55:41 -0300

    btn de add, delete e clear em uma listView

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,11 +12,26 @@ namespace WpfApp2
         {
             InitializeComponent();
             lvEntries.Items.Add("a");
-            lvEntries.Items.Add("a1");
-            lvEntries.Items.Add("a2");
-            lvEntries.Items.Add("a3");
-            lvEntries.Items.Add("a4");
+            lvEntries.Items.Add("b");
+            lvEntries.Items.Add("c");
+            lvEntries.Items.Add("d");
         }
 
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Add(txtEntry.Text);
+            txtEntry.Clear();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int index = lvEntries.SelectedIndex;
+            lvEntries.Items.RemoveAt(index);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Clear();
+        }
     }
 }
